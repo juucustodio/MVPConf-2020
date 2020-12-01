32171bb5c4656c9f24bc9c1a0257fd50bbf12947
Juliano Custódio <juliano.custodio@hotmail.com.br>
2020-12-01 18:58:32 -0300

    Cria projeto com layout tabed

---
+++ MVPConfApp/MVPConfApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MVPConfApp.ViewModels;
+using MVPConfApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MVPConfApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ MVPConfApp/MVPConfApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MVPConfApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MVPConfApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
