333548d0c9bf17cab38ac786254449c6c479c637
Juliano Custódio <juliano.custodio@hotmail.com.br>
2020-12-03 15:19:52 -0300

    update about page

--- MVPConfApp/MVPConfApp.Android/Resources/Resource.designer.cs
+++ MVPConfApp/MVPConfApp.Android/Resources/Resource.designer.cs
@@ -8385,13 +8385,16 @@ namespace MVPConfApp.Droid
 			public const int notify_panel_notification_icon_bg = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int tooltip_frame_dark = 2131165302;
+			public const int pessoas = 2131165302;
 			
 			// aapt resource value: 0x7F070077
-			public const int tooltip_frame_light = 2131165303;
+			public const int tooltip_frame_dark = 2131165303;
 			
 			// aapt resource value: 0x7F070078
-			public const int xamarin_logo = 2131165304;
+			public const int tooltip_frame_light = 2131165304;
+			
+			// aapt resource value: 0x7F070079
+			public const int xamarin_logo = 2131165305;
 			
 			static Drawable()
 			{
